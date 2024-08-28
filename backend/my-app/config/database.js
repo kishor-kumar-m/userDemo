66c8868abf475c547c4c94a4a779fb8ccc569018
kishor <kishorkumarmahalingam@gmail.com>
2024-08-28 15:21:50 +0530

    fixed warning

--- backend/my-app/config/database.js
+++ backend/my-app/config/database.js
@@ -4,8 +4,6 @@ const config = require('./config');
 const connectDB = async () => {
   try {
     await mongoose.connect(config.mongoURI ? config.mongoURI : "mongodb://localhost:27017/", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('MongoDB Connected Successfully');
   } catch (err) {
